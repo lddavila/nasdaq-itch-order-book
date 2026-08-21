bd9e21073c3d0f65e4f3eadaa896ea8e722f2da3
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-08-20 21:23:27 -0600

    added download and modified so that downloaded data remains after container is closed

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import yaml
 import databento as db
 from dotenv import load_dotenv
+from pathlib import Path
+import argparse
 
 def initialize_config(yaml_path="config.yaml"):
     """Loads environment variables and parses the nested configuration file."""
@@ -23,16 +25,40 @@ def initialize_config(yaml_path="config.yaml"):
         
     with open(yaml_path, "r") as file:
         return yaml.safe_load(file)
+def test_pipeline():
+    print("--- Starting System Diagnostics ---")
+    
+    # 1. Test .env mapping
+    load_dotenv()
+    api_key = os.getenv("DATABENTO_API_KEY")
+    print(f"[1/4] Checking Environment Variables... Found Key: {bool(api_key)}")
+    
+    # 2. Test config.yaml structural mapping
+    def env_var_constructor(loader, node):
+        return os.getenv(loader.construct_scalar(node), "")
+    yaml.SafeLoader.add_constructor("!env", env_var_constructor)
+    
+    with open("config.yaml", "r") as file:
+        config = yaml.safe_load(file)
+    print(f"[2/4] Parsing config.yaml structure... App Name: {config['app']['name']}")
 
+    # 3. Test Databento Connection & Package Architecture
+    print("[3/4] Connecting to Databento Servers...")
+    client = db.Historical(key=api_key)
+
+    output_path = Path("/app/raw_data/AAPL_2026-07-01_mbo.dbn.zst")
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+    
+    # 4. request some data to verify the connection
+    print("[4/4] Requesting sandbox dataset...")
+    data = client.timeseries.get_range(
+    dataset="XNAS.ITCH",
+    schema="mbo",
+    symbols=["AAPL"],
+    start="2026-07-01T13:30:00",
+    end="2026-07-01T20:00:00")
+
+    print(f"\nSUCCESS! Network handshake complete.")
+    data.to_file(output_path)
 if __name__ == "__main__":
-    # Bootstrapping the configuration mapping
-    config = initialize_config()
-
-    # Accessing config data directly
-    app_name = config["app"]["name"]
-    api_key = config["services"]["weather_api"]["api_key"]
-    db_pass = config["database"]["password"]
-
-    print(f"--- Launching {app_name} ---")
-    print(f"API Key successfully retrieved: {bool(api_key)} (Length: {len(api_key)})")
-    print(f"Database password loaded into RAM: {bool(db_pass)}")
+    test_pipeline()
