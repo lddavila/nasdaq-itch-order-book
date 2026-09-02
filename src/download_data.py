75a35419e36e1905e9e1c553f3f5a9edebfa8861
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2026-09-01 23:31:46 -0600

    fixed bug that caused downloaded data to ignore the midnight snapshot

--- src/download_data.py
+++ src/download_data.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 import os
 from pathlib import Path
 import databento as db
-def download_stock_symbol_data(symbol,date,output,start_time,end_time):
+from datetime import date, timedelta
+def download_stock_symbol_data(symbol,date_string,output,start_time,end_time):
     
     """Downloads market data for a specific stock symbol and date."""
     # Load environment variables
@@ -18,23 +19,30 @@ def download_stock_symbol_data(symbol,date,output,start_time,end_time):
     # Define the output path
     if os.path.exists(output):
         print(f"Warning: The output file {output} already exists and the download will be skipped. Please specify a different output path if you wish to redownload the data.")
-    else:
-        output_path = Path(output)
-        output_path.parent.mkdir(parents=True, exist_ok=True)
-        # Request data from Databento
-        try:
-            data = client.timeseries.get_range(
-                dataset="XNAS.ITCH",
-                schema="mbo",
-                symbols=[symbol],
-                start=f"{date}{start_time}",
-                end=f"{date}{end_time}"
-            )
-        except Exception as e:
-            print(f"Error downloading data for {symbol} on {date}: {e}")
-            return
-        print(f"successfully downloaded data to destination {output_path}.")
-    
-        # Save the data to the specified output file
-        data.to_file(output_path)
-        print(f"Data for {symbol} on {date} has been downloaded to {output_path}")
+        return
+
+    trading_day = date.fromisoformat(date_string)
+    following_day = trading_day + timedelta(days=1)
+
+    start = f"{trading_day.isoformat()}T00:00:00Z"
+    end = f"{following_day.isoformat()}T00:00:00Z"
+
+    output_path = Path(output)
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+    # Request data from Databento
+    try:
+        data = client.timeseries.get_range(
+            dataset="XNAS.ITCH",
+            schema="mbo",
+            symbols=[symbol],
+            start=start,
+            end=end
+        )
+    except Exception as e:
+        print(f"Error downloading data for {symbol} on {date_string}: {e}")
+        return
+    print(f"successfully downloaded data to destination {output_path}.")
+
+    # Save the data to the specified output file
+    data.to_file(output_path)
+    print(f"Data for {symbol} on {date} has been downloaded to {output_path}")
